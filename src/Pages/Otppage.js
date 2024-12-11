64b90ec660b99e4ca82c6c4b873d71438842d9e9
manishatamang752@gmail.com <manishatamang752@gmail.com>
2024-12-11 21:44:09 +0545

    Opt page made

--- src/Pages/Otppage.js
+++ src/Pages/Otppage.js
@@ -1,48 +1,53 @@
-// import React, { useState } from "react";
+import React, { useState } from "react";
 
-// const Otppage = () => {
-//   const [otp, setOtp] = useState(["", "", "", "", "", ""]);
-//   const [errMsg, setErrMsg] = useState("");
+const Otppage = () => {
+  const [otp, setOtp] = useState(["", "", "", "", "", ""]);
+  const [Msg, setMsg] = useState("");
 
-//   const handleChange = (e, index) => {
-//     const value = e.target.value;
-//     if (!isNaN(value) && value.length <= 1) {
-//       const newotp = [...otp];
-//       newotp[index] = value;
-//       setOtp(newotp);
-//       if (value !== "" && e.target.nextSibling) {
-//         e.target.nextSibling.onFocus();
-//       }
-//     }
-//   };
+  const handleChange = (e, index) => {
+    const value = e.target.value;
+    if (!isNaN(value) && value.length <= 1) {
+      const newotp = [...otp];
+      newotp[index] = value;
+      setOtp(newotp);
+      if (value !== "" && e.target.nextSibling) {
+        e.target.nextSibling.onFocus();
+      }
+    }
+  };
 
-//   const handlesub = (e) => {
-//     e.preventDefault();
-//     const enteredOtp = otp.join(""); //to join all otp
-//   };
-//   return (
-//     <div className="otp-page">
-//       <h2>Enter OTP</h2>
-//       <form onSubmit={handleSubmit} className="otp-form">
-//         <div className="otp-inputs">
-//           {otp.map((value, index) => (
-//             <input
-//               type="numbers"
-//               maxLength="1"
-//               value={value}
-//               onChange={(e) => handleChange(e.target, index)}
-//               onFocus={(e) => e.target.select()}
-//               className="otp-input"
-//             />
-//           ))}
-//         </div>
-//         <button type="submit" className="otp-submit">
-//           Submit
-//         </button>
-//       </form>
-//       {message && <p className="otp-message"></p>}
-//     </div>
-//   );
-// };
+  const handlesub = (e) => {
+    e.preventDefault();
+    const enteredOtp = otp.join(""); //to join all otp
+    if (enteredOtp.length === 6) {
+      setMsg(`otp sunmitted :${enteredOtp}`);
+    } else {
+      setMsg("try again");
+    }
+  };
+  return (
+    <div className="otp-page">
+      <h2>Enter OTP</h2>
+      <form onSubmit={handlesub} className="otp-form">
+        <div className="otp-inputs">
+          {otp.map((value, index) => (
+            <input
+              type="numbers"
+              maxLength="1"
+              value={value}
+              onChange={(e) => handleChange(e.target, index)}
+              onFocus={(e) => e.target.select()}
+              className="otp-input"
+            />
+          ))}
+        </div>
+        <button type="submit" className="otp-submit">
+          Submit
+        </button>
+      </form>
+      {Msg && <p className="otp-message">{Msg}</p>}
+    </div>
+  );
+};
 
-// export default Otppage;
+export default Otppage;
